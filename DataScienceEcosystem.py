91963a37e1496b6473fce4337a5e22f3c49e2cc7
John Basha <111846041+janbasha-07@users.noreply.github.com>
2023-04-14 10:56:42 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>DataScienceEcosystem</h1>
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# <ol>
+#     <li>Python</li>
+#     <li>R</li>
+#     <li>SQL</li>
+# </ol>
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# <ol>
+#     <li>Pandas</li>
+#     <li>Numpy</li>
+#     <li>Sklearn</li>
+# </ol>
+
+# <table>
+#     <tr>
+#         <td>Data Science Tools</td>
+#     </tr>
+# </table>
+
+# <h1>Below are a few examples of evaluating arithmetic expressions in Python</h1>
+
+# In[4]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# <b>Objectives:</b>
+# List popular languages for Data Science
+# <ul>
+#     <li>Python</li>
+#     <li>R</li>
+#     <li>SQL</li>
+#     <li>Java</li>
+#     <li>Scala</li>
+# </ul>
+
+# <h2>Author</h2>
+# JohnBasha
+
+# In[ ]:
+
+
+
+
